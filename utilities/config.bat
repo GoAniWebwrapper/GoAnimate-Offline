5084e4a1298d11bdcda31704b39de2dbe3a40604
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-03-14 09:41:37 -0400

    Update config.bat

--- utilities/config.bat
+++ utilities/config.bat
@@ -1,4 +1,4 @@
-:: Wrapper: Offline Config
+:: GoAnimate Offline Config
 :: This file is modified by settings.bat. It is not organized, but comments for each setting have been added.
 :: You should be using settings.bat, and not touching this. Offline relies on this file remaining consistent, and it's easy to mess that up.
 
